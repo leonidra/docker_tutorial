65dc1d411f9f20f5958abc562fbe9f7b7854d5db
Leonid Rashkovsky <leonid.r@fiverr.com>
2016-02-01 00:03:22 +0200

    A lot of changed for the app

--- app/app.rb
+++ app/app.rb
@@ -5,8 +5,16 @@ Bundler.require(:default)
 
 include Mongo
 
+set :bind, '0.0.0.0'
+set :port, 9292
+
 configure do
-  host = ENV["VBOX_IP"] || "localhost"
+  if ENV['DOCKER_ENV'].nil?
+    host = ENV["VBOX_IP"] || "localhost"
+  else
+    host = 'db'
+  end
+
   database = 'devops'
   coll = 'devops_quotes'
 
@@ -20,6 +28,12 @@ get '/' do
   random_num = rand(1..settings.mongo_db.find().count).to_i
   quote = settings.mongo_db.find().limit(-1).skip(random_num).to_a.first
 
-  quote['id'] = quote['_id'].to_s
-  { :id => quote['id'], :quote => quote[:quote] }.to_json
+  if quote.nil?
+    response = { :error => "No quotes found"}
+  else
+    quote['id'] = quote['_id'].to_s
+    response = { :id => quote['id'], :quote => quote[:quote] }
+  end
+
+  response.to_json
 end
